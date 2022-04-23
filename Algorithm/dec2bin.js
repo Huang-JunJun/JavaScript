2ebbaac1a900452ba350509a1d9b4c632b71f0b7
黄显竣 <770894595@qq.com>
2022-04-24 03:30:55 +0800

    十进制转二进制

---
+++ Algorithm/dec2bin.js
@@ -0,0 +1,12 @@
+const dec2bin = (num) => {
+    const stack = []
+
+    while (num > 0) {
+        stack.push(num % 2)
+        num = Math.floor(num / 2)
+    }
+
+    return stack.reverse().join('')
+}
+
+console.log(dec2bin(100));
