05d742245396571b4339658d47eb4892acd3bf0e
黄显竣 <770894595@qq.com>
2022-04-29 12:46:08 +0800

    链表结构的封装

---
+++ DataStructure/LinkList.js
@@ -0,0 +1,106 @@
+// 链表结构
+function LinkList() {
+    // 节点类
+    function Node(val) {
+        this.val = val
+        this.next = null
+    }
+
+    this.head = null
+    this.length = 0
+
+    LinkList.prototype.append = (element) => {
+        const node = new Node(element)
+        if (this.length === 0) {
+            this.length++
+           return this.head = node
+        }
+
+        let curNode = this.head
+        while(curNode.next) {
+            curNode = curNode.next
+        }
+
+        curNode.next = node
+        this.length++
+    }
+
+    LinkList.prototype.insert = (element, index) => {
+        if (index < 0) return;
+
+        if(this.head === null) {
+            if (!index) {
+                this.length++
+                return this.head = element
+            } else {
+                return
+            }
+        }
+
+        const node = new Node(element)
+        let curNode = this.head
+        let preNode = this.head.next
+        let pos = 0
+
+        while(preNode.next) {
+            if (pos == index) {
+                curNode.next = node
+                node.next = preNode
+                break
+            }
+            curNode = preNode
+            preNode = preNode.next
+            pos++
+        }
+
+        this.length++
+    }
+
+    LinkList.prototype.findNode = (index) => {
+        let curNode = this.head
+        let curIndex = 0
+        if(this.length === 0) {
+            return this.head
+        } else {
+            while (curNode.next) {
+                if (curIndex === index) {
+                    return curNode
+                }
+                curIndex++
+                curNode = curNode.next
+            }
+        }
+    }
+
+    LinkList.prototype.delete = (index) => {
+        let curNode = this.head
+        let curIndex = 0
+        if(this.length === 0) {
+            return
+        } else {
+            while (curNode.next) {
+                if (curIndex === index - 1) {
+                    curNode.next = curNode.next.next
+                    this.length--
+                    break
+                }
+                curIndex++
+                preNode = curNode
+                curNode = curNode.next
+            }
+        }
+    }
+}
+
+const list = new LinkList()
+
+list.append('123')
+list.append('456')
+list.append('789')
+
+list.insert('1010', 0)
+
+console.log(list);
+console.log(list.findNode(1));
+list.delete(1)
+console.log(list);
