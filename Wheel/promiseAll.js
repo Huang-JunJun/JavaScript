95794b7e5cff513d6ac17d4147329cecb0d51fc3
huangjunjun <770894595@qq.com>
2022-11-05 05:01:24 +0800

    手写promise.all

---
+++ Wheel/promiseAll.js
@@ -0,0 +1,36 @@
+// 手写promise.all
+const promiseAll = (promises) => {
+  // 只接受数组
+  return new Promise((resolve, reject) => {
+    if(!Array.isArray(promises)) return reject(new Error('传入的参数不是数组！'))
+    let results = []
+    for (let i = 0; i < promises.length; i++) {
+      Promise.resolve(promises[i]).then(value => {
+        results[i] = value
+        if(results.length === promises.length) {
+          resolve(results)
+        }
+      }).catch(e => reject(e))
+    }
+  })
+}
+
+let p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('p1 resolved')
+  }, 1000)
+})
+
+let p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('p2 resolved')
+  }, 2000)
+})
+
+// Promise.all([p1, p2]).then(data => {
+//   console.log(data);
+//   p1.then(state => console.log(state))
+// })
+promiseAll([p1, p2]).then(data => {
+  console.log(data);
+})
