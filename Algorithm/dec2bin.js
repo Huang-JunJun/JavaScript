1e8c7fc215ba74cf8abcec7a2ad0389fe4f77886
huangjunjun <770894595@qq.com>
2022-11-23 00:39:47 +0800

    十进制转二进制

--- Algorithm/dec2bin.js
+++ Algorithm/dec2bin.js
@@ -2,8 +2,8 @@ const dec2bin = (num) => {
     const stack = []
 
     while (num > 0) {
-        stack.push(num % 2)
-        num = Math.floor(num / 2)
+      stack.push(num % 2)
+      num = Math.floor(num / 2)
     }
 
     return stack.reverse().join('')
