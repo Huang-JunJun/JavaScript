215be2d2d482e1daf9cf6f0b870bd96f75da6d73
huangjunjun <770894595@qq.com>
2022-12-02 06:34:34 +0800

    盛水最多的容器

---
+++ Algorithm/maxArea.js
@@ -0,0 +1,30 @@
+const maxArea = (height) => {
+  let max = 0
+  // 遇到小的就去除
+
+  while(height.length > 0) {
+    let l = 0
+    let r = height.length - 1
+    let sum = 0
+    if(height[l] < height[r]){
+      sum = height[l]*(height.length-1)
+      height.shift()
+    } else if (height[r] < height[l]){
+      sum = height[r]*(height.length-1)
+      height.pop()
+    } else {
+      sum = height[l]*(height.length-1)
+      height.pop()
+    }
+
+    if(sum > max){
+      max = sum
+    }
+  }
+
+  return max
+}
+
+const height = [1,8,6,2,5,4,8,3,7]
+
+console.log(maxArea([1,1]));
