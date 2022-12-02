2c95707006a0897de3c43d26c5b7636fe40e94f9
huangjunjun <770894595@qq.com>
2022-12-02 21:04:03 +0800

    爬楼梯

--- Algorithm/climbStairs.js
+++ Algorithm/climbStairs.js
@@ -3,14 +3,24 @@
  * @return {number}
  */
 const climbStairs = (n) => {
-  if(n === 1) {
-    return 1
-  }
-  if(n === 2) {
-    return 2
-  }
+  // 递归超时
+  // if(n === 1) {
+  //   return 1
+  // }
+  // if(n === 2) {
+  //   return 2
+  // }
 
-  return climbStairs(n-1) + climbStairs(n-2)
+  // return climbStairs(n-1) + climbStairs(n-2)
+  let q = 0
+  let p = 0
+  let r = 1
+  for (let i = 0; i < n; i++) {
+    p = q
+    q = r
+    r = q + p
+  }
+  return r
 };
 
 console.log(climbStairs(10));
