25aa80144e66717c5e5b85c2fc17349bfc59f7ec
huangjunjun <770894595@qq.com>
2022-12-06 22:05:27 +0800

    存在重复元素

---
+++ Algorithm/containsDuplicate.js
@@ -0,0 +1,9 @@
+const containsDuplicate = (nums) => {
+  for (let i = 0; i < nums.length; i++) {
+    if(nums.indexOf(nums[i]) !== nums.lastIndexOf(nums[i])){
+      return true
+    }
+  }
+
+  return false
+}
